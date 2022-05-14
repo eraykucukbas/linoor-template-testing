d929e908d78b93955f331059fcdf1901faf389de
Eray Volkan Küçükbaş <87654248+eraykucukbas@users.noreply.github.com>
2022-05-14 14:22:51 +0300

    Update BlogPage.vue

--- files/components/BlogPage.vue
+++ files/components/BlogPage.vue
@@ -1,37 +1,29 @@
 <template>
-  <section class="news-section">
-    <div class="auto-container">
-      <div>
+  <section>
+    <div>
+      <div id="card">
+        <ul class="list-group">
+          <li class="list-group-item" v-for="character in characterList" :key="character.id">
+            <img class="image" v-bind:src="character.image">
+            <h1 id="title">{{character.id}}-{{character.name}} </h1>
+            <p>Status : {{character.status}}</p>
+            <p>Species : {{character.species}}</p>
+            <p>Gender : {{character.gender}}</p>
+          </li>
+        </ul>
       </div>
-      
-      
-      <div class="row clearfix">
-
-        <div class="card">
-          <ul class="list-group" style="display:flex">
-            <li class="list-group-item" v-for="character in characterList" :key="character.id">
-              <img v-bind:src="character.image" style="width:100%">
-              <h4> {{character.id}}-{{character.name}} </h4>
-              <h5>Status : {{character.status}}</h5>
-              <h5>Species : {{character.species}}</h5>
-              <h5>Gender : {{character.gender}}</h5>
-            </li>
-          </ul>
-        </div>
-
-      </div>
-
-
-
     </div>
+
+
   </section>
+
+
 </template>
 
 <script>
   import test from '../plugins/helper'
   export default {
     name: "BlogPage",
-
     data() {
       return {
         characterImage: null,
@@ -43,31 +35,65 @@
         characterList: {}
       }
     },
-
     methods: {
-
       getCharacters() {
-
         this.$http.get("https://rickandmortyapi.com/api/character")
           .then((response) => {
-
             this.characterList = response.data.results;
-
           })
       }
-
-
-
     },
-        created: function(){
-        this.getCharacters()
+    created: function () {
+      this.getCharacters()
     }
   }
 
 </script>
 
 <style scoped>
+  div {
+    display: inline-block;
+    margin: 5%;
+  }
+
+  ul {
+    display: inline-block;
+  }
+
+  li {
+    display: inline-block;
+    list-style: none;
+    margin: 2%;
+    padding: 0%;
+    box-sizing: border-box;
+    border: 1px solid black;
+    border-radius: 1%;
+    background-color: bisque;
+  }
+
+  .image {
+    display: inline-block;
+  }
 
 
+
+  #title {
+    color: rgb(155, 70, 235);
+    font-family: fantasy;
+
+  }
+
+  p {
+    color: rgb(30, 204, 102);
+    font-weight: bold;
+
+  }
+
+  h1,
+  p {
+    font-size: 100%;
+    margin: 1%;
+    text-align: center;
+  }
 
 </style>

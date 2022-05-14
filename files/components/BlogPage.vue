a674dba06036fcf63afbaafb3c7f0e658e1aac71
Eray Volkan Küçükbaş <87654248+eraykucukbas@users.noreply.github.com>
2022-05-14 14:23:30 +0300

    Update BlogPage.vue

--- files/components/BlogPage.vue
+++ files/components/BlogPage.vue
@@ -12,7 +12,27 @@
           </li>
         </ul>
       </div>
+<<<<<<< Updated upstream
     </div>
+=======
+
+
+      <div class="row clearfix">
+
+
+
+        <div class="card">
+          <ul class="list-group" style="display:flex">
+            <li class="list-group-item" v-for="character in characterList" :key="character.id">
+              <img v-bind:src="character.image" style="width:100%">
+              <h4> {{character.id}}-{{character.name}} </h4>
+              <h5>Status : {{character.status}}</h5>
+              <h5>Species : {{character.species}}</h5>
+              <h5>Gender : {{character.gender}}</h5>
+            </li>
+          </ul>
+        </div>
+>>>>>>> Stashed changes
 
 
   </section>
